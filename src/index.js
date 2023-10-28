8896059c927fdbbe3bddb24da0342b4c8ef9c95f
krantagat <kranta.g@gmail.com>
2023-10-28 18:32:32 +0200

    Add time adjust css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
+function formatDate(timestamp) {
+  let dateData = new Date(timestamp);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let day = days[dateData.getDay()];
+  let date = dateData.getDate();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[dateData.getMonth()];
+  let year = dateData.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
+
+function formatTime(timestamp) {
+  let dateData = new Date(timestamp);
+  let hour = dateData.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = dateData.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${hour}:${minute}`;
+}
+
 function mainTemperature(response) {
   let temperaureData = document.querySelector("#temperature");
   let cityData = document.querySelector("#city");
+
   let realFeelData = document.querySelector("#realFeel");
   let windData = document.querySelector("#wind");
   let humidityData = document.querySelector("#humidity");
   let descriptionData = document.querySelector("#description");
+  let dateData = document.querySelector("#date");
+  let timeData = document.querySelector("#time");
   temperaureData.innerHTML = Math.round(response.data.main.temp);
   cityData.innerHTML = `${response.data.name},${response.data.sys.country}`;
   realFeelData.innerHTML = `Realfeel: ${Math.round(
@@ -13,11 +54,15 @@ function mainTemperature(response) {
   windData.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
   humidityData.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   descriptionData.innerHTML = response.data.weather[0].description;
-  console.log(response.data.weather[0].description);
+
+  dateData.innerHTML = formatDate(response.data.dt * 1000);
+  timeData.innerHTML = formatTime(response.data.dt * 1000);
+
+  console.log(apiUrl);
 }
 
 // let city = prompt
-let city = "Paris";
+let city = "Aguascalientes";
 let apiKey = "ff1d9ea9376b5c27a82e04fc2b2abdbb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
