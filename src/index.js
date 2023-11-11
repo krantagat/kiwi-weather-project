cf1892cb39ee826dd1a8a987ebb4b3558a1eb258
krantagat <kranta.g@gmail.com>
2023-11-11 16:11:09 +0100

    Current location
    Add current location button
    Add js for current location
    Clear search bar when search or click current location

--- src/index.js
+++ src/index.js
@@ -128,7 +128,7 @@ function searchCity(event) {
   event.preventDefault();
   let cityData = document.querySelector("#search-city");
   search(cityData.value);
-  console.log(cityData.value);
+  document.getElementById("search-city").value = "";
 }
 
 // function displayFahrenheitTemperature(event) {
@@ -166,3 +166,52 @@ form.addEventListener("submit", searchCity);
 // celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Lille");
+
+// Update city from currentLocation
+function showCurrentLocationTemp(currentLocation) {
+  currentLocation.preventDefault();
+  document.getElementById("search-city").value = "";
+
+  function handlePosition(position) {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    let apiKey = "21ofd8a07570t9fa235b2705bf347e82";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${long}&lat=${lat}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showCurrentLocationData);
+  }
+
+  function showCurrentLocationData(response) {
+    let temperatureData = document.querySelector("#temperature");
+    let cityData = document.querySelector("#city");
+    let realFeelData = document.querySelector("#realFeel");
+    let windData = document.querySelector("#wind");
+    let humidityData = document.querySelector("#humidity");
+    let descriptionData = document.querySelector("#description");
+    let dateData = document.querySelector("#date");
+    let dateLastUpdateData = document.querySelector("#dateLastUpdate");
+    let timeLastUpdateData = document.querySelector("#timeLastUpdate");
+    let iconData = document.querySelector("#icon");
+
+    celsiusTemperature = Math.round(response.data.temperature.current);
+    temperatureData.innerHTML = celsiusTemperature;
+    cityData.innerHTML = `${response.data.city}, ${response.data.country}`;
+    celsiusRealFeel = Math.round(response.data.temperature.feels_like);
+    realFeelData.innerHTML = `Realfeel: ${celsiusRealFeel}°`;
+    windData.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+    humidityData.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
+    descriptionData.innerHTML = response.data.condition.description;
+    dateData.innerHTML = formatDate(response.data.time * 1000);
+    dateLastUpdateData.innerHTML = formatDate(response.data.time * 1000);
+    timeLastUpdateData.innerHTML = formatTime(response.data.time * 1000);
+
+    iconData.setAttribute(
+      "src",
+      `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    );
+    getForecast(response.data.city);
+  }
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", showCurrentLocationTemp);
